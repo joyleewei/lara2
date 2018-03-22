b6111bf4a7915b73795d5c09019f03ade9f481f0
joyleewei <1515885616@qq.com>
2018-03-22 20:46:08 +0800

    finish layout and routes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller{
     public function create(){
-
         return view('users.create');
     }
+
+    // 个人信息中心 依赖注入，隐性路由模型绑定
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+
 }
